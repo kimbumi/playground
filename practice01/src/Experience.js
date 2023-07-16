2e08c2a5c3a6948df0f87e947f1d0a1731726813
kimbumi <bumikim26@gmail.com>
2023-07-16 15:31:08 +0900

    learn r3f-animation

--- practice01/src/Experience.js
+++ practice01/src/Experience.js
@@ -1,8 +1,26 @@
+import { useFrame } from "@react-three/fiber"
+import { useRef } from "react"
+
 export default function Experience() {
+  useFrame(()=>{
+    redCubeRef.current.rotation.y += 0.01;
+  })
+  const redCubeRef = useRef()
+  
   return <>
-    <mesh scale={2} position-x={Math.PI * 0.5}>
-        <sphereGeometry args={[1,32,32]}/>
-        <meshBasicMaterial color="hotpink"/>
-    </mesh>
+    <group>
+      <mesh  scale={2} position-x={2} >
+        <boxGeometry/>
+        <meshBasicMaterial color="blue"/>
+      </mesh>
+      <mesh ref={redCubeRef} scale={1} position-x={-2} >
+        <boxGeometry/>
+        <meshBasicMaterial color="red"/>
+      </mesh>
+      <mesh rotation-x={Math.PI/-2} position-y={-1}>
+        <planeGeometry args={[10,10]}/>
+        <meshBasicMaterial color="greenyellow"/>
+      </mesh>
+    </group>
   </>
 }
