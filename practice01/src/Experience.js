84dd211ac88753fbc84714fd5750cc81a0712ff5
kimbumi <bumikim26@gmail.com>
2023-07-17 20:33:58 +0900

    learn r3f-useThree to access the state

--- practice01/src/Experience.js
+++ practice01/src/Experience.js
@@ -1,26 +1,35 @@
-import { useFrame } from "@react-three/fiber"
+import { useFrame, useThree, extend } from "@react-three/fiber"
 import { useRef } from "react"
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+
+extend({OrbitControls})
 
 export default function Experience() {
-  useFrame(()=>{
-    redCubeRef.current.rotation.y += 0.01;
+  useFrame((state, delta)=>{
+    cubeRef.current.rotation.y += delta
+    // groupRef.current.rotation.y += delta
   })
-  const redCubeRef = useRef()
-  
+  const cubeRef = useRef()
+  const groupRef = useRef()
+
+
+  const threeState = useThree()
+
   return <>
-    <group>
+    <orbitControls args={[threeState.camera, threeState.gl.domElement]}/>
+    <group ref={groupRef}>
       <mesh  scale={2} position-x={2} >
         <boxGeometry/>
         <meshBasicMaterial color="blue"/>
       </mesh>
-      <mesh ref={redCubeRef} scale={1} position-x={-2} >
+      <mesh ref={cubeRef} scale={1} position-x={-2} >
         <boxGeometry/>
         <meshBasicMaterial color="red"/>
       </mesh>
-      <mesh rotation-x={Math.PI/-2} position-y={-1}>
+    </group>
+    <mesh rotation-x={Math.PI/-2} position-y={-1}>
         <planeGeometry args={[10,10]}/>
         <meshBasicMaterial color="greenyellow"/>
-      </mesh>
-    </group>
+    </mesh>
   </>
 }
